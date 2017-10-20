fa6ab5c0dde3c7f6a210d837828bca2b24ee6759
Chad Harrington <chad@deercreeklabs.com>
2017-10-20 00:19:39 -0600

    lein-release plugin: bumped version from 0.1.1 to 0.1.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deercreeklabs/log-utils "0.1.1"
+(defproject deercreeklabs/log-utils "0.1.2-SNAPSHOT"
   :description "A logging and debug library for Clojure and ClojureScript."
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
